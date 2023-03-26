4aab90ff4983ef16f875baa28649ad5a03961c40
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-03-26 23:33:03 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// const mainDiv = document.querySelector(".main");
+
+//https://api.imgflip.com/get_memes
+//http://i.imgflip.com/1bij.jpg
+
+const topTextInput = document.querySelector(".topText");
+
+// console.log(topTextInput);
+
+topTextInput.addEventListener("keyup", (e) => {
+  console.log("what");
+});
